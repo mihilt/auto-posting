812d0ba59d345bdc551d8a02771db9df370bf3b1
mihilt <50368758+mihilt@users.noreply.github.com>
2020-07-06 15:00:40 +0900

    met json error

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import json
 import time
+import pyperclip
 
-#네이버 카페 주소 바로 입력 기준이다
-
-with open("secret/account.json") as json_file:
+with open("secret/data.json") as json_file:
     json_data = json.load(json_file)
     data_id = json_data["id"]
     data_pw = json_data["pw"]
-    data_address = json_data["address"]
+
+    data_address = json_data["address"] #카페 대문 주소
+
+    data_category_choice = json_data["category_choice"]  # 게시판 콤보박스 요소 선택
+    data_post_subject = json_data["post_subject"] #게시글 제목
+
+
 
 
 #저장된 account 확인
@@ -23,10 +29,28 @@ driver = webdriver.Chrome()
 driver.get(data_address)
 driver.find_element_by_xpath('//*[@id="gnb_login_button"]/span[3]').click()
 
-driver.find_element_by_name('id').send_keys(data_id)
+# driver.find_element_by_name('id').send_keys(data_id)
+pyperclip.copy(data_id)
+# driver.find_element_by_name('id').click()
+driver.find_element_by_name('id').send_keys(Keys.CONTROL, 'v')
+
 time.sleep(1)
-driver.find_element_by_name('pw').send_keys(data_pw)
+# driver.find_element_by_name('pw').send_keys(data_pw)
+pyperclip.copy(data_pw)
+# driver.find_element_by_name('pw').click()
+driver.find_element_by_name('pw').send_keys(Keys.CONTROL, 'v')
+
 time.sleep(1)
 driver.find_element_by_xpath('//*[@id="log.login"]').click()
 
-# 일단 오늘 여기서 네이버 캡챠가 떠버림..
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="cafe-info-data"]/div[2]/a').click()
+
+#게시판 선택
+driver.find_element_by_xpath('//*[@id="boardCategory"]/option[text()=' + data_category_choice + ']').click()
+
+# 제목입력
+driver.find_element_by_name('subject').send_keys(data_post_subject)
+
+
+#캡챠 해결, JSON 한글 파싱 오류 생김
